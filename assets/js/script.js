2f33cef5f8559fe61fd5daa1f7caab36cefd54f1
Josh8903 <thorngren87@gmail.com>
2020-10-06 11:19:21 -0500

    laid out results card and implemented append function on search

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,33 @@ $("#search").click(function() {
     var searchGenre = $(".searchGenre").val();
     localStorage.setItem("searchTitle", searchTitle);
     localStorage.setItem("searchGenre", searchGenre);
+    run();
 });
+
+
+// appends result card, placeholder text assigned to result vars 
+function run(searchTitle) {
+    var art = "art",
+        title = "title",
+        releaseDate = "release date",
+        author = "author",
+        genre = "genre",
+        plot = "plot";
+
+    $("#results").append(`
+    <div id="resultCard" class="pure-g">
+        <div id="art" class="pure-u-1-5">
+            ${art}
+        </div>
+        <div id="info" class="pure-u-4-5">
+        <ul>
+            <li id="title">${title}</li>
+            <li id="releaseDate">${releaseDate}</li>
+            <li id="author">${author}</li>
+            <li id="genre">${genre}</li>
+            <li id="plot">${plot}</li>
+        </ul>
+        </div>
+    </div>
+    `);
+}
