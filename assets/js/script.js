50d225dedcbe7521d566993842ab38775c397378
Josh8903 <thorngren87@gmail.com>
2020-10-06 11:38:09 -0500

    result card fully implemented, last search on load implemented, just need AJAX

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,42 @@
-// local storage - onclick #search button - last search valur for title and genre saved
+// local storage - onclick #search button - last search value for title and genre saved
 $("#search").click(function() {
     event.preventDefault()
     var searchTitle = $(".searchTitle").val();
     var searchGenre = $(".searchGenre").val();
     localStorage.setItem("searchTitle", searchTitle);
     localStorage.setItem("searchGenre", searchGenre);
-    run();
+    runSearch(searchTitle, searchGenre);
 });
+
+// on load run last search
+window.onload = function() {
+    var searchTitle = localStorage.getItem("searchTitle");
+    var searchGenre = localStorage.getItem("searchGenre");
+    runSearch(searchTitle, searchGenre);
+};
 
 
 // appends result card, placeholder text assigned to result vars 
-function run(searchTitle) {
+function runSearch(searchTitle, searchGenre) {
     var art = "art",
-        title = "title",
+        title = `Title ${searchTitle}`,
         releaseDate = "release date",
         author = "author",
-        genre = "genre",
+        genre = `Genre ${searchGenre}`,
         plot = "plot";
 
-    $("#results").append(`
-    <div id="resultCard" class="pure-g">
-        <div id="art" class="pure-u-1-5">
-            ${art}
+    $("#results").prepend(`
+        <div id="resultCard" class="pure-g">
+            <div id="art" class="pure-u-1-5">${art}</div>
+            <div id="info" class="pure-u-4-5">
+            <ul>
+                <li id="title">${title}</li>
+                <li id="releaseDate">${releaseDate}</li>
+                <li id="author">${author}</li>
+                <li id="genre">${genre}</li>
+                <li id="plot">${plot}</li>
+            </ul>
+            </div>
         </div>
-        <div id="info" class="pure-u-4-5">
-        <ul>
-            <li id="title">${title}</li>
-            <li id="releaseDate">${releaseDate}</li>
-            <li id="author">${author}</li>
-            <li id="genre">${genre}</li>
-            <li id="plot">${plot}</li>
-        </ul>
-        </div>
-    </div>
     `);
 }
