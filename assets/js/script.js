c59b0808badb36b91afad6efc575a2c2d55bdd4e
Charline <charlinerlach@gmail.com>
2020-10-06 12:54:13 -0500

    Added some lowerCase and + changes to .js, plus added a title ID on HTML.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 // local storage - onclick #search element - last text entry saved to local storage item "searchTerm"
 $("#search").click(function() {
     event.preventDefault()
-    var searchTerm = $("input").val();
+    var searchTerm = $("input").val().toLowerCase();
+    console.log(searchTerm);
+    var spaceReplace = searchTerm.split(' ').join('+');
+    console.log(spaceReplace);
     localStorage.setItem("searchTerm", searchTerm);
+    localStorage.setItem("spaceReplace", spaceReplace);
 });
